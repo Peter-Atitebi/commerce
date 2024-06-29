516c2a7edfeb785c983ee6232de0c02920b7c53a
Atitebi Oyinkansola Peter <132858902+Peter-Atitebi@users.noreply.github.com>
2024-06-29 10:05:08 +0100

    Add files via upload
    Yh

---
+++ components/Extracontent.jsx
@@ -0,0 +1,259 @@
+import React from "react";
+
+const Extracontent = () => {
+  return (
+    <div>
+      <div className="extra-content">
+        <div className="extras-appliances">
+          <div className="first-extras">
+            <h2>Small Appliances</h2>
+            <ul>
+              <li>Blenders</li>
+              <li>Deep Fryers</li>
+              <li>Air Fryers</li>
+              <li>Rice Cookers</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Large Appliances</h2>
+            <ul>
+              <li>Washing Machines</li>
+              <li>Fridges</li>
+              <li>Freezers</li>
+              <li>Generators</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="extras-computing">
+          <div className="first-extras">
+            <h2>Computers</h2>
+            <ul>
+              <li>Desktops</li>
+              <li>Laptops</li>
+              <li>Macbooks</li>
+              <li>Chromebooks</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Data Storage</h2>
+            <ul>
+              <li>External Hard Drives</li>
+              <li>USB Flash Drives</li>
+              <li>External Solid State</li>
+              <li>USB Card Readers</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Printers</h2>
+            <ul>
+              <li>Laser Printers</li>
+              <li>Inkjet Printers</li>
+              <li>Printer Ink</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Computer Accessories</h2>
+            <ul>
+              <li>Keyboards &#36; Mice</li>
+              <li>PC Gaming Hardware</li>
+              <li>Memory Cards</li>
+              <li>Uninterrupted Power Supply</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="extras-electronics">
+          <div className="first-extras">
+            <h2>Televisions</h2>
+            <ul>
+              <li>Smart TVs</li>
+              <li>LED &#36; LCD TVs</li>
+              <li>TV Accessories</li>
+              <li>Curved TVs</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Cameras</h2>
+            <ul>
+              <li>Digital Cameras</li>
+              <li>Projectors</li>
+              <li>CCTV Cameras</li>
+              <li>Drones with Cameras</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Home Audio</h2>
+            <ul>
+              <li>Home Theatre Systems</li>
+              <li>Sound Bars</li>
+              <li>Amplifiers</li>
+              <li>Bluetooth Speakers</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Generators &#36; Portable Power</h2>
+            <ul>
+              <li>Generators</li>
+              <li>Power Inverters</li>
+              <li>Solar &#36; Wind Power</li>
+              <li>Stabilizers</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="extras-gaming">
+          <div className="first-extras">
+            <h2>Play Station</h2>
+            <ul>
+              <li>PS5</li>
+              <li>PS4</li>
+              <li>PS3</li>
+              <li>PlayStation VIta</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>XBOX</h2>
+            <ul>
+              <li>XBOX One</li>
+              <li>XBOX 360</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Nintendo</h2>
+            <ul>
+              <li>Nintendo 3DS</li>
+              <li>Nintendo DS</li>
+              <li>Nintendo Switch</li>
+              <li>Nintendo Wii</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Top Games</h2>
+            <ul>
+              <li>FIFA</li>
+              <li>Call Of Duty</li>
+              <li>Grand Theft Auto</li>
+              <li>The Witcher 3</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="extras-phones">
+          <div className="first-extras">
+            <h2>Mobile Phones</h2>
+            <ul>
+              <li>Smartphones</li>
+              <li>iPhones</li>
+              <li>Android Phones</li>
+              <li>Basic Phones</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Tablets</h2>
+            <ul>
+              <li>Android Tablets</li>
+              <li>iPads</li>
+              <li>Educational Tablets</li>
+              <li>Tablet Accessories</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Mobile Accessories</h2>
+            <ul>
+              <li>Adapters</li>
+              <li>Bluetooth Headsets</li>
+              <li>Earbuds, Airpods, &#36; Headsets</li>
+              <li>Powerbanks</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="extras-wearables">
+          <div>
+            <h2>Fitness Trackers</h2>
+          </div>
+
+          <div>
+            <h2>Humane AI Pin</h2>
+          </div>
+
+          <div>
+            <h2>Fitbit</h2>
+          </div>
+
+          <div>
+            <h2>Pixel Watch</h2>
+          </div>
+        </div>
+
+        <div className="extras-smart-tech">
+          <div>
+            <h2>Smart Home Lighting</h2>
+            <ul>
+              <li>Light Bulbs</li>
+              <li>Strip Lighting</li>
+              <li>Dimmers &#36; Switches</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Smart Locks &#36; Entry</h2>
+          </div>
+
+          <div>
+            <h2>Smart Devices</h2>
+          </div>
+
+          <div>
+            <h2>Voice Assistants</h2>
+          </div>
+
+          <div>
+            <h2>Smart TVs</h2>
+          </div>
+        </div>
+
+        <div className="extras-apps">
+          <div>
+            <h2>Lake Technologies Best</h2>
+          </div>
+
+          <div>
+            <h2>Healthcare</h2>
+          </div>
+
+          <div>
+            <h2>Travel</h2>
+          </div>
+
+          <div>
+            <h2>Finance</h2>
+          </div>
+
+          <div>
+            <h2>Books</h2>
+          </div>
+
+          <div>
+            <h2>Programming Services</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Extracontent;
